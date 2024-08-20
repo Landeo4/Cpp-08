d6d6249d8cf4ef4324c0056517312ecc8538cad6
Theo POTILLION <tpotilli@made-f0Br6s19.clusters.42paris.fr>
2024-08-20 13:36:27 +0200

    Ex00 finis

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -4,19 +4,22 @@
 #include <iostream>
 #include <stack>
 
-template <typename T>
-class MutantStack
+template <typename T, class container=std::deque<T> >
+class MutantStack: public std::stack<T, container>
 {
     public:
-    MutantStack();
-    ~MutantStack();
-    MutantStack(const MutantStack & copy);
-    MutantStack&operator=(const MutantStack & copy);
 
-    Begin();
-    end();
+        MutantStack() {}
+        ~MutantStack() {}
+        MutantStack(MutantStack &copy) {(void)copy;}
+        MutantStack &operator = (MutantStack &src) {std::stack<T, container>::operator=(src);return *this;}
+
+        typedef typename container::iterator iterator;
+        iterator begin() {return (this->c.begin());}
+        iterator end() {return (this->c.end());}
+
     private:
 
-}   ;
+};
 
 #endif
